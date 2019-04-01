998f21d8451504651c30ff97c432f2ce6d02e114
Brian Thornton <brian.thornton@cerner.com>
2019-04-01 06:44:43 -0400

    Created project

---
+++ adapters/data/file-adapter.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+
+class FileAdapter {
+  constructor(storageLocation) {
+    this.storageLocation = storageLocation;
+    FileAdapter.createIfNotExists(this.storageLocation);
+  }
+
+  write(type, id, object) {
+    FileAdapter.createIfNotExists(path.join(this.storageLocation, type));
+    fs.writeFileSync(path.join(this.storageLocation, type, `${id}.json`), JSON.stringify(object, null, 2));
+  }
+
+  get(type, id) {
+    return JSON.parse(fs.readFileSync(path.join(this.storageLocation, type, `${id}.json`)));
+  }
+
+  getAll(type) {
+    const data = [];
+    const files = fs.readdirSync(path.join(this.storageLocation, type));
+    files.forEach((file) => {
+      data.push(this.get(type, file.replace('.json', '')));
+    });
+
+    return data;
+  }
+
+  remove(type, id) {
+    fs.unlinkSync(path.join(this.storageLocation, type, `${id}.json`));
+  }
+
+  clear(type) {
+    const files = fs.readdirSync(path.join(this.storageLocation, type));
+    files.forEach((file) => {
+      fs.unlinkSync(path.join(this.storageLocation, type, file));
+    });
+  }
+
+  update(type, id, object) {
+    this.remove(type, id);
+    this.write(type, id, object);
+  }
+
+  static createIfNotExists(folder) {
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+  }
+}
+module.exports = FileAdapter;
---
+++ index.js
@@ -0,0 +1,10 @@
+const DataAccess = require('./lib/data-access');
+const Librarian = require('./lib/librarian');
+
+module.exports = {
+  dataAccess: DataAccess,
+  librarian: Librarian,
+};
+
+// lint
+// ./node_modules/.bin/eslint
