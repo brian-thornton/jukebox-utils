6ebacbb3c270afa7058565639ceb48f9aa86e6c5
brian-thornton <psutron@gmail.com>
2019-04-28 06:48:29 -0400

    Added enqueueTop

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const DataAccess = require('./lib/data-access');
 const Librarian = require('./lib/librarian');
 const Player = require('./lib/player');
 const Volume = require('./lib/volume');
+const Queue = require('./lib/queue');
 
 module.exports = {
   dataAccess: DataAccess,
   librarian: Librarian,
   player: Player,
   volume: Volume,
+  queue: Queue,
 };
 
 // lint
